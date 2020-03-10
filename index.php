c7cbb7178e74536f6a64c03a2a2771137c40de63
GuidingD <alek1@mail.ru>
2020-03-10 22:47:54 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML><HEAD>
+<meta http-equiv="Content-Type" content="text/html; charset=Windows-1251" />
+
+<title>Услуги по дизайну аквариумов. Москва.</title>
+<LINK rel=stylesheet href="/alesta-fish/images/styles.css">
+<BODY>
+<div id="panel"></div>
+<DIV style="BACKGROUND: url(/alesta-fish/images/aquarium.gif) no-repeat 95% 80%">
+<TABLE id=maintable border=0 cellSpacing=0 cellPadding=0 width=899 align=center 
+height="100%">
+  <TBODY>
+  <TR>
+    <TD><!-- HEADER -->
+      <TABLE style="MARGIN-BOTTOM: 50px" border=0 cellSpacing=0 cellPadding=0
+      width="100%">
+        <TBODY>
+        <TR>
+          <TD vAlign=top>
+            <TABLE border=0 cellSpacing=0 cellPadding=0 width="100%">
+              <TBODY>
+              <TR>
+                <TD vAlign=top width="30%"><A
+                  href="#"><IMG border=0
+                  alt="Alesta-fish — водный мир без хлопот!"
+                  src="/alesta-fish/images/logo.jpg"
+                  width=224 height=166></A>
+                  <P>&nbsp;</P></TD>
+                </TR>
+              <TR>
+                <TD vAlign=top width="100%" colSpan=5>
+                  <P align=left><BR><B><FONT style="FONT-SIZE: 12pt">+7 (495)
+                  723-22-29</FONT></B></P></TD></TR></TBODY></TABLE></TD></TR></TBODY></TABLE>
+  <TR>
+    <TD height="100%" vAlign=top><!-- MAIN -->
+      <TABLE border=0 cellSpacing=0 cellPadding=0 width="100%" height="100%">
+        <TBODY>
+        <TR>
+          <TD vAlign=top width=5 align=middle></TD>
+          <TD style="PADDING-RIGHT: 20px; PADDING-TOP: 37px" vAlign=top 
+          align=middle>
+            <DIV 
+            style="BACKGROUND-IMAGE: url(/alesta-fish/images/fotos.gif); WIDTH: 193px; HEIGHT: 410px">
+            <DIV 
+            style="TEXT-ALIGN: center; FONT-FAMILY: Arial; COLOR: #7f91ab; FONT-SIZE: 12px; PADDING-TOP: 20px">Примеры 
+            наших работ<BR><A href="#"><IMG 
+            border=0 alt="Пример дизайна морского аквариума" vspace=12 
+            src="/alesta-fish/images/pic1.jpg" 
+            width=168 height=90></A> <A 
+            href="#"><IMG border=0 
+            alt="Пример дизайна аквариума" vspace=0 
+            src="/alesta-fish/images/pic2.jpg" 
+            width=168 height=99></A> <A 
+            href="#"><IMG border=0 
+            alt="Пример дизайна пресноводного аквариума" vspace=12 
+            src="/alesta-fish/images/pic3.jpg" 
+            width=168 height=97></A><BR><A class=fotos 
+            href="#">Посмотреть еще 
+            работы</A> </DIV></DIV></TD>
+          <TD height=300 vAlign=top>
+
+ <H2 style="TEXT-ALIGN: left; COLOR: #fac152" class=he1>О нас.</H2>
+            <P>У нас вы сможете заказать профессиональные услуги по 
+            аквариумистике. Квалификация наших работников позволит Вам навсегда 
+            забыть о проблемах с аквариумом.</P>
+            <P>Мы предоставляем гарантию на все услуги (подробнее в разделе цены).</P>
+            <P>Если Вы еще не обзавелись аквариумом, а только собираетесь, то 
+            можете подобрать его в нашем каталоге или
+            заказать по своим размерам. Наши специалисты установят и оформят с 
+            учетом Ваших пожеланий, подключат необходимое оборудование и при 
+            необходимости поставят на ежемесячное обслуживание.</P>
+            <P>Мы ценим своих клиентов. Индивидуальный подход к каждому 
+            позволяет нам воплощать самые сложные и интересные проекты клиентов. 
+            Оплата услуг производится в соответствии с заключаемым договором в 
+            наличной или безналичной форме, а оформить заказ вы можете заполнив 
+            соответствующую
+            форму на сайте или позвонив по указанному внизу страницы
+            телефону.</P><BR>
+            <H2 style="TEXT-ALIGN: left" class=he1>Мы предоставляем следующие 
+            услуги </H2>
+            <P><B>Обслуживание аквариумов</B><BR>Профессиональное обслуживание
+            аквариума обеспечит Вашим питомцам безупречный вид и отличное
+            самочувствие.</P>
+            <P><B>Индивидуальный дизайн аквариума</B><BR>Аквариум, выполненный в 
+            индивидуальном дизайне, станет прекрасным<BR>дополнением к любому 
+            интерьеру. Мы с радостью произведем оформление
+            аквариумов.</P>
+            <P>Аквариумы<BR>В
+            нашем каталоге представлены наиболее распространенные виды 
+            аквариумов.</P>
+            <P><B>Статьи:</B><BR>Так же на нашем сайте вы найдете статьи по 
+            дизайну и обслуживанию<BR>аквариумов</P><BR>
+            <H2 style="TEXT-ALIGN: left" class=he1>Полезная информация</H2>
+            <P>Почему стоит
+            обращаться за помощью к специалистам?<BR>Чем следует
+            руководствоваться при выборе компании,<BR>предоставляющей услуги по 
+            аквариумистике.</P><BR>
+
+			</TD></TR></TBODY></TABLE>
+
+
+  <TR>
+    <TD vAlign=top align=right><!-- FOOTER --><DIV id=niz><BR><SPAN style="COLOR: #fac152">+7 (495) 723-22-29</SPAN><BR><A style="COLOR: #6f99dd" href="#">alesta-fish@list.ru</A><BR>Адрес: 109369, г. Москва, ул. Перерва д. 56/2<br />2006-2020 © <A href="#">Алеста—фиш</A> Москва. Все права защищены.<BR><BR></DIV>
+	  
+      </TR></TBODY></TABLE>
+
+</DIV><!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter25495934 = new Ya.Metrika({id:25495934, webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/25495934" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
+</BODY></HTML>
